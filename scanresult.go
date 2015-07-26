bb493ef1b9f3838f5aa9afb9b5a5c684ccbb4d71
Grim <megaskyhawk@gmail.com>
2015-07-26 19:32:10 -0400

    Initial Commit

---
+++ scanresult.go
@@ -0,0 +1,255 @@
+package godb
+
+// Copyright 2015 Just Another Organization
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import (
+	"fmt"
+	"io"
+	"reflect"
+	"strconv"
+	"sync"
+	"time"
+)
+
+// TODO Remove all of the `fmt.Print*`
+
+//ScanResult represents a scan. So if results.Next() { results.scan()} is ran multiple times, there will be multiple of these.
+type ScanResult struct {
+	sync.RWMutex // embedded.  see http://golang.org/ref/spec#Struct_types
+	FieldPtrArr  []interface{}
+	FieldArr     []interface{}
+	FieldCount   int64
+	MapFieldToID map[string]int64
+}
+
+func (results *ScanResult) put(k string, v int64) {
+	results.RLock()
+
+	defer results.RUnlock()
+	results.MapFieldToID[k] = v
+}
+
+// Get ...
+func (results *ScanResult) Get(k string) interface{} {
+	results.RLock()
+	defer results.RUnlock()
+	// TODO: check map key exist and wrapper.FieldArr boundary.
+	return results.FieldArr[results.MapFieldToID[k]]
+}
+
+// PutFields ...
+func (results *ScanResult) PutFields(fArr []string) {
+	fCount := len(fArr)
+	results.FieldArr = make([]interface{}, fCount)
+	results.FieldPtrArr = make([]interface{}, fCount)
+	results.MapFieldToID = make(map[string]int64, fCount)
+
+	for k, v := range fArr {
+		results.FieldPtrArr[k] = &results.FieldArr[k]
+		results.put(v, int64(k))
+	}
+}
+
+//Warning, not Locked.
+func (results *ScanResult) getVal(key string) (interface{}, bool) {
+	results.RLock()
+	defer results.RUnlock()
+	if _, ok := results.MapFieldToID[key]; ok {
+		return results.FieldArr[results.MapFieldToID[key]], true
+	}
+	return nil, false
+}
+
+// GetFieldPtrArr ...
+func (results *ScanResult) GetFieldPtrArr() []interface{} {
+	return results.FieldPtrArr
+}
+
+// GetFieldArr ...
+func (results *ScanResult) GetFieldArr() map[string]interface{} {
+	m := make(map[string]interface{}, results.FieldCount)
+
+	for k, v := range results.MapFieldToID {
+		m[k] = results.FieldArr[v]
+	}
+
+	return m
+}
+
+//GetString returns a string for the specified key in the result
+func (results *ScanResult) GetString(key string) string {
+	if val, ok := results.getVal(key); ok {
+		switch val2 := val.(type) {
+		case []uint8:
+			b := make([]byte, len(val2))
+			for i, v := range val2 {
+				b[i] = byte(v)
+			}
+			return string(b)
+		case string:
+			return val2
+		case int64:
+			return fmt.Sprintf("%v", val2)
+		case nil:
+			return ""
+		default:
+			fmt.Printf("Failed type assertion for GetString(%v)\nType: %v\n", key, reflect.TypeOf(val))
+			return ""
+		}
+	}
+	return ""
+}
+
+//CheckString returns a string and a bool indicating if the string was correctly parsed for the specified key in the result
+func (results *ScanResult) CheckString(key string) (string, bool) {
+	if val, ok := results.getVal(key); ok {
+		switch val2 := val.(type) {
+		case []uint8:
+			b := make([]byte, len(val2))
+			for i, v := range val2 {
+				b[i] = byte(v)
+			}
+			return string(b), true
+		case string:
+			return val2, true
+		case int64:
+			return fmt.Sprintf("%v", val2), true
+		case nil:
+			return "", true //Return (nothing, true) because technically it's not there? This means that there technically wasn't an error.
+		default:
+			fmt.Printf("Failed type assertion for CheckString(%v)\nType: %v\n", key, reflect.TypeOf(val))
+			return "", false
+		}
+	}
+	return "", false
+}
+
+// GetInterface ...
+func (results *ScanResult) GetInterface(key string) (i interface{}) {
+	var ok bool
+	if i, ok = results.CheckString(key); ok {
+		return
+	}
+	if i, ok = results.CheckInt(key); ok {
+		return
+	}
+	if i, ok = results.CheckBool(key); ok {
+		return
+	}
+	return
+}
+
+//GetBool returns a bool for the specified key in the result
+// TODO Add more type checks.
+func (results *ScanResult) GetBool(key string) bool {
+	if val, ok := results.getVal(key); ok {
+		if val2, ok := val.(int64); !ok {
+			fmt.Printf("GetBool failed Type Assertion! Type is %v\n", reflect.TypeOf(val))
+		} else {
+			return val2 > 0
+		}
+	}
+	return false
+}
+
+//CheckBool returns a bool and if the bool was correctly parsed for the specified key in the result
+// TODO Add more type checks.
+func (results *ScanResult) CheckBool(key string) (bool, bool) {
+	if val, ok := results.getVal(key); ok {
+		if val2, ok := val.(int64); !ok {
+			fmt.Printf("CheckBool failed Type Assertion! Type is %v\n", reflect.TypeOf(val))
+		} else {
+			return val2 > 0, true
+		}
+
+	}
+	return false, false
+}
+
+//GetInt returns an int for the specified key in the result
+func (results *ScanResult) GetInt(key string) int {
+	if val, ok := results.getVal(key); ok {
+		switch val2 := val.(type) {
+		case []uint8:
+
+			b := make([]byte, len(val2))
+			for i, v := range val2 {
+				b[i] = byte(v)
+			}
+
+			num, err := strconv.Atoi(string(b))
+			if err != nil {
+				fmt.Printf("Error on GetInt(%v): %v\n", key, err)
+			}
+			return num
+		case string:
+			i, _ := strconv.Atoi(string(val2))
+			return i
+		case int64:
+			return int(val2)
+		case nil:
+			return 0
+		default:
+			fmt.Printf("GetInt(%v) returned an unknown type: %v\n", key, reflect.TypeOf(val))
+
+		}
+	}
+	return 0
+}
+
+//CheckInt returns an int and a bool indicating if the int was correctly parsed for the specified key in the result
+// TODO Add more type checks.
+func (results *ScanResult) CheckInt(key string) (int, bool) {
+	if val, ok := results.getVal(key); ok {
+		if val2, ok := val.(int64); !ok {
+			fmt.Printf("CheckInt failed Type Assertion! Type is %v\n", reflect.TypeOf(val))
+		} else {
+			return int(val2), true
+		}
+	}
+	return 0, false
+}
+
+//TryToClose tries to close a closable object.
+func TryToClose(obj io.Closer) {
+	if err := obj.Close(); err != nil {
+		fmt.Printf("Memory leak created by failure to close an object! %v\n", err) //TODO Fix import path so that it can be logged.
+	}
+}
+
+var supportedTimes = []string{
+	"2006-01-02 15:04:5",
+	"2006-01-02 15:04:5 -0700 MST",
+	time.RFC3339,
+	time.RFC822,
+	time.RFC1123,
+	time.RFC1123Z,
+}
+
+func stringToTimePtr(t string) *time.Time {
+	if t == "" {
+		return &time.Time{}
+	}
+	var tim time.Time
+	var err error
+	for _, timeFormat := range supportedTimes {
+		tim, err = time.Parse(timeFormat, t)
+		if err == nil {
+			return &tim
+		}
+	}
+	fmt.Printf("Unsupported time string: `%v`\n", t)
+	return nil
+}
---
+++ wrapper.go
@@ -0,0 +1,563 @@
+package godb
+
+// Copyright 2015 Just Another Organization
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import (
+	"database/sql"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"reflect"
+	"strconv"
+	"time"
+
+	geo "github.com/paulmach/go.geo"
+)
+
+// ReflectTypes
+var (
+	timePType = reflect.TypeOf(&time.Time{})
+	geoPoint  = reflect.TypeOf(&geo.Point{})
+	geoPath   = reflect.TypeOf(&geo.Path{})
+)
+
+//NewWrapper because one less line is amazing.
+func NewWrapper(database *sql.DB) Wrapper {
+	var wrapper Wrapper
+	wrapper.Wrap(database)
+	return wrapper
+}
+
+//Wrapper wraps around querys.
+type Wrapper struct {
+	ScanResults []*ScanResult
+	Cursor      int //Should start at 0
+	CurrentScan int //Should start at -1
+	Connection  *sql.DB
+	Transaction *sql.Tx
+}
+
+// RowCount returns the amount of scan results.
+func (wrapper *Wrapper) RowCount() int {
+	if wrapper == nil {
+		return 0
+	}
+	return len(wrapper.ScanResults)
+}
+
+// Begin starts the transaction
+func (wrapper *Wrapper) Begin() (err error) {
+	wrapper.Transaction, err = wrapper.Connection.Begin()
+	return
+}
+
+// Commit commits the transaction
+func (wrapper *Wrapper) Commit() error {
+	if err := wrapper.Transaction.Commit(); err != nil {
+		fmt.Printf("Failed to commit transaction: %v\n", err)
+		return err
+	}
+	wrapper.Transaction = nil
+	return nil
+}
+
+// Revert rolls back the transaction
+func (wrapper *Wrapper) Revert() error {
+	if err := wrapper.Transaction.Rollback(); err != nil {
+		fmt.Printf("Failed to row back failed transacton: %v\n", err)
+		return err
+	}
+	wrapper.Transaction = nil
+	return nil
+}
+
+// Next is a dirty attempt at replicating scan.Next
+func (wrapper *Wrapper) Next() bool {
+	defer func() { wrapper.Cursor++; wrapper.CurrentScan++ }() //Each time this is ran, make the cursor indicate the next scan result.
+	return wrapper.Cursor < len(wrapper.ScanResults)           //If cursor == 0, then it will be less than 1 if there is only one result.
+}
+
+// Prepare prepares the statement.
+func (wrapper *Wrapper) Prepare(statement string) (*sql.Stmt, error) {
+	if wrapper.Transaction != nil {
+		return wrapper.Transaction.Prepare(statement)
+	}
+	return wrapper.Connection.Prepare(statement)
+}
+
+// Execute executes the statement with the params, and returns last inserted id, and the rows affected.
+// TODO Remove TryToClose. Return error instead.
+func (wrapper *Wrapper) Execute(statementString string, params ...interface{}) (int64, int64, error) {
+	statement, err := wrapper.Prepare(statementString)
+	if err != nil {
+		fmt.Printf("Failed to prepare\nError: %v\nStatement: %v\nValues: %v\n", err, statementString, params)
+		return 0, 0, err
+	}
+	defer TryToClose(statement)
+
+	results, err := statement.Exec(params...)
+	if err != nil {
+		fmt.Printf("Failed to exec\nError: %v\nStatement: %v\nValues: %v\n", err, statementString, params)
+		return 0, 0, err
+	}
+
+	lID, err := results.LastInsertId()
+	if err != nil {
+		fmt.Printf("Failed to get last inserted id\nError: %v\nStatement: %v\nValues: %v\n", err, statementString, params)
+		return 0, 0, err
+	}
+
+	rwA, err := results.RowsAffected()
+	if err != nil {
+		fmt.Printf("Failed to get rows affected\nError: %v\nStatement: %v\nValues: %v\n", err, statementString, params)
+		return 0, 0, err
+	}
+
+	return lID, rwA, nil
+}
+
+//Clears the history.
+func (wrapper *Wrapper) clear() {
+	wrapper.ScanResults = nil
+	wrapper.CurrentScan = -1
+	wrapper.Cursor = 0
+}
+
+//Wrap wraps around the database to use.
+func (wrapper *Wrapper) Wrap(db *sql.DB) {
+	wrapper.Connection = db
+	wrapper.CurrentScan = -1
+}
+
+//Query queries the query string, with whatever params given.
+// TODO Remove TryToClose. Return error instead.
+func (wrapper *Wrapper) Query(queryStatement string, params ...interface{}) error {
+	wrapper.clear()
+	statement, err := wrapper.Prepare(queryStatement)
+	if err != nil {
+		fmt.Printf("wrapper.Prepare resulted in an error.\nParams: %v\nError: %v\n", queryStatement, err)
+		return err
+	}
+	defer TryToClose(statement)
+
+	results, err := statement.Query(params...)
+	if err != nil {
+		// TODO Debug
+		fmt.Printf("statement.query resulted in an error.\nParams: %v\nError: %v\n", queryStatement, err)
+		return err
+	}
+	defer TryToClose(results)
+
+	var fArr []string
+	if fArr, err = results.Columns(); err != nil {
+		fmt.Printf("results.Columns() resulted in an error.\nParams: %v\nError: %v\n", queryStatement, err)
+		return err
+	}
+
+	for count := 0; results.Next(); count++ {
+		var result ScanResult
+		result.PutFields(fArr)
+		if err = results.Scan(result.GetFieldPtrArr()...); err != nil {
+			fmt.Printf("results.Scan resulted in an error.\nParams: %v\nError: %v\n", queryStatement, err)
+			return err
+		}
+		wrapper.ScanResults = append(wrapper.ScanResults, &result)
+	}
+
+	return nil
+}
+
+//QueryOne queries the query string, with whatever params given, gives back one value as an interface.
+// TODO Remove TryToClose. Return error instead.
+func (wrapper *Wrapper) QueryOne(queryStatement string, params ...interface{}) (i interface{}, err error) {
+	defer func() {
+		if err != nil {
+			fmt.Printf("Failed to query once\nError: %v\nStatement: %v\n", err, queryStatement)
+		}
+	}()
+	wrapper.clear()
+	statement, err := wrapper.Prepare(queryStatement)
+	if err != nil {
+		return
+	}
+	defer TryToClose(statement)
+
+	results, err := statement.Query(params...)
+	if err != nil {
+		return
+	}
+	defer TryToClose(results)
+
+	var fArr []string
+	if fArr, err = results.Columns(); err != nil || !results.Next() {
+		return
+	}
+	if len(fArr) > 1 {
+		err = errors.New("May only return one value with this function")
+		return
+	}
+
+	var sr ScanResult
+	sr.PutFields(fArr)
+	if err = results.Scan(sr.GetFieldPtrArr()...); err != nil {
+		return
+	}
+
+	i = sr.GetInterface(fArr[0])
+
+	return
+}
+
+func (wrapper *Wrapper) getVal(key string) (interface{}, bool) {
+	current, err := wrapper.Current()
+	if err != nil {
+		return nil, false
+	}
+	return current.getVal(key)
+}
+
+//Current returns the current row(ScanResult)
+func (wrapper *Wrapper) Current() (*ScanResult, error) {
+	wrapper.doubleCheckCurrentScanCursor()
+	if len(wrapper.ScanResults) < 1 {
+		return nil, errors.New("No Scan result found")
+	}
+	if len(wrapper.ScanResults)-1 < wrapper.CurrentScan {
+		return nil, errors.New("Ran out of Scan results")
+	}
+	return wrapper.ScanResults[wrapper.CurrentScan], nil
+}
+
+// DumpResults ...
+func (wrapper *Wrapper) DumpResults() {
+	for _, results := range wrapper.ScanResults {
+		results.Lock()
+		fmt.Print("Keys: ")
+		for key := range results.MapFieldToID {
+			fmt.Print(key + " ")
+		}
+		fmt.Println()
+		results.Unlock()
+	}
+}
+
+//GetInt ...
+func (wrapper *Wrapper) GetInt(key string) int {
+	current, err := wrapper.Current()
+	if err != nil {
+		return 0
+	}
+	return current.GetInt(key)
+}
+
+//CheckInt ...
+func (wrapper *Wrapper) CheckInt(key string) (int, bool) {
+	current, err := wrapper.Current()
+	if err != nil {
+		return 0, false
+	}
+	return current.CheckInt(key)
+}
+
+//GetBool ...
+func (wrapper *Wrapper) GetBool(key string) bool {
+	current, err := wrapper.Current()
+	if err != nil {
+		return false
+	}
+	return current.GetBool(key)
+}
+
+//CheckBool ...
+func (wrapper *Wrapper) CheckBool(key string) (bool, bool) {
+	current, err := wrapper.Current()
+	if err != nil {
+		return false, false
+	}
+	return current.CheckBool(key)
+}
+
+// GetInterface ...
+func (wrapper *Wrapper) GetInterface(key string) (i interface{}) {
+	current, err := wrapper.Current()
+	if err != nil {
+		return
+	}
+	return current.GetInterface(key)
+}
+
+//GetString ...
+func (wrapper *Wrapper) GetString(key string) string {
+	current, err := wrapper.Current()
+	if err != nil {
+		return ""
+	}
+	return current.GetString(key)
+}
+
+//CheckString ...
+func (wrapper *Wrapper) CheckString(key string) (string, bool) {
+	current, err := wrapper.Current()
+	if err != nil {
+		return "", false
+	}
+	return current.CheckString(key)
+}
+
+//Unmarshal is a way to umarshal into an object. The field that you are unmarshaling to MUST be a string.
+// TODO check the fields we have, not the fields in the struct....
+func (wrapper *Wrapper) Unmarshal(i interface{}) error {
+	return wrapper.unmarshal(reflect.ValueOf(i).Elem())
+}
+
+func (wrapper *Wrapper) unmarshal(v reflect.Value) error {
+	t := v.Type()
+	for i := 0; i < v.NumField(); i++ {
+		field := v.Field(i)
+		tag := t.Field(i).Tag.Get("sql")
+		if tag != "" {
+			switch field.Kind() {
+			case reflect.Int:
+				ptr := field.Addr().Interface().(*int)
+				*ptr = wrapper.GetInt(tag)
+			// case reflect.Int64:
+			// 	ptr := field.Addr().Interface().(*int64)
+			// 	*ptr = wrapper.GetInt64(tag)
+			case reflect.String:
+				ptr := field.Addr().Interface().(*string)
+				*ptr = wrapper.GetString(tag)
+			case reflect.Bool:
+				ptr := field.Addr().Interface().(*bool)
+				*ptr = wrapper.GetBool(tag)
+			case reflect.Float64:
+				ptr := field.Addr().Interface().(*float64)
+				*ptr, _ = strconv.ParseFloat(wrapper.GetString(tag), 64)
+
+				// Magic Reflection
+			case reflect.Slice, reflect.Array:
+				i := field.Interface()
+				if err := json.Unmarshal([]byte(wrapper.GetString(tag)), &i); err != nil {
+					break
+				}
+				a2v := reflect.ValueOf(i)
+				if !a2v.IsValid() {
+					break
+				}
+				a2vLen := a2v.Len()
+				if a2vLen > 0 {
+					field.Set(reflect.MakeSlice(field.Type(), a2vLen, a2vLen))
+					for i := 0; i < a2vLen; i++ {
+						if a2v.Index(i).Elem().Type().ConvertibleTo(field.Type().Elem()) {
+							field.Index(i).Set(a2v.Index(i).Elem().Convert(field.Type().Elem()))
+						}
+					}
+				}
+				// End Magic Reflection
+
+			case reflect.Struct, reflect.Ptr:
+				switch field.Type() {
+				case timePType:
+					ptr := field.Addr().Interface().(**time.Time)
+					*ptr = stringToTimePtr(wrapper.GetString(tag))
+
+					// Magic Geo Points....
+				case geoPoint:
+					val, ok := wrapper.getVal(tag)
+					if ok {
+						var byts []byte
+						// Uh since this is so unsafe, I decided to wrap it just incase...
+						// Reasons why it's unsafe:
+						// - Assuming type is []uint8
+						// - Assuming data type is WKT
+						// - Assuming I know what the type is.
+						func() {
+							defer func() {
+								if err := recover(); err != nil {
+									fmt.Printf("WKB vs WKT issue...... %v\n", err)
+								}
+							}()
+
+							byts = make([]byte, len(val.([]uint8)))
+							for i, v := range val.([]uint8) {
+								byts[i] = byte(v)
+							}
+							if len(byts) > 4 {
+
+								// ¯\_(ツ)_/¯
+								byts = byts[4:]
+							}
+
+						}()
+						// Lets pretend, that byts is correct and not think about it kkthx.
+						ptr := field.Addr().Interface().(**geo.Point)
+						*ptr = geo.NewPointFromWKB(byts)
+					}
+
+				case geoPath:
+					val, ok := wrapper.getVal(tag)
+					if ok {
+						var byts []byte
+						// Uh since this is so unsafe, I decided to wrap it just incase...
+						// Reasons why it's unsafe:
+						// - Assuming type is []uint8
+						// - Assuming data type is WKT
+						// - Assuming I know what the type is.
+						func() {
+							defer func() {
+								if err := recover(); err != nil {
+									fmt.Printf("WKB vs WKT issue...... %v\n", err)
+								}
+							}()
+
+							byts = make([]byte, len(val.([]uint8)))
+							for i, v := range val.([]uint8) {
+								byts[i] = byte(v)
+							}
+							if len(byts) > 4 {
+
+								// ¯\_(ツ)_/¯
+								byts = byts[4:]
+							}
+
+						}()
+						// Lets pretend, that byts is correct and not think about it kkthx.
+						ptr := field.Addr().Interface().(**geo.Path)
+						*ptr = geo.NewPathFromWKB(byts)
+					}
+
+				default:
+					fmt.Printf("Unsupported Wrapper Type: %v\tTag: %v\n", field.Type(), tag)
+				}
+			default:
+				fmt.Printf("Unsupported Kind: %v\tTag: %v\n", field.Kind(), tag)
+			}
+		}
+	}
+	return nil
+}
+
+//HasResults is used to ensure that there are results waiting. Because certain things panic some reason...
+func (wrapper *Wrapper) HasResults() bool {
+	return len(wrapper.ScanResults) > 0
+}
+
+//Used incase someone forgets to call wrapper.Next(). Not the best thing, but needed to avoid a panic.
+func (wrapper *Wrapper) doubleCheckCurrentScanCursor() {
+	if wrapper.CurrentScan == -1 {
+		wrapper.CurrentScan = 0
+	}
+}
+
+// UnmarshalTo ...
+func (wrapper *Wrapper) UnmarshalTo(key string, i interface{}) (err error) {
+	if wrapper.GetString(key) == "" {
+		return
+	}
+	err = json.Unmarshal([]byte(wrapper.GetString(key)), i)
+	if err != nil {
+		return
+	}
+	return
+}
+
+// ArrayOfThings ...
+type ArrayOfThings interface{}
+
+// Function ...
+type Function interface{}
+
+// Unwrap will allocate the array, and unmarshal to it.
+func (wrapper *Wrapper) Unwrap(array ArrayOfThings, function ...Function) (err error) {
+	value := reflect.ValueOf(array)
+	if value.IsNil() {
+		err = errors.New("Wrapper.Unwrap: Cannot unwrap into a nil value, you probably forgot the `&`")
+		return
+	}
+
+	var f interface{}
+	if function != nil {
+		if len(function) > 0 {
+			f = function[0]
+		}
+	}
+
+	value = value.Elem()
+	value.Set(reflect.MakeSlice(reflect.TypeOf(array).Elem(), wrapper.RowCount(), wrapper.RowCount()))
+
+	if wrapper.RowCount() > 0 {
+		if err = validateFunction(value.Index(0), f); err != nil {
+			return
+		}
+	} else {
+		return
+	}
+
+	for i := 0; wrapper.Next(); i++ {
+		val := value.Index(i)
+		if val.Kind() != reflect.Struct {
+			err = fmt.Errorf("Wrapper.Unwrap: unsupported type for unwrapping: `%v`\nMust use a structed object", val.Kind())
+			return
+		}
+		if err = wrapper.unmarshal(val); err != nil {
+			return
+		}
+		if err = injectFunction(val, f); err != nil {
+			return
+		}
+	}
+
+	return
+}
+
+func validateFunction(value reflect.Value, f interface{}) (err error) {
+	if f == nil {
+		return
+	}
+	if reflect.TypeOf(f).Kind() != reflect.Func {
+		err = fmt.Errorf("Wrapper.Unwrap: provided function is not a function, but rather a `%v`", reflect.TypeOf(f).Kind())
+		return
+	}
+
+	fun := reflect.ValueOf(f)
+	if fun.Type().NumIn() != 1 {
+		// unsupported number of parameters.
+		err = fmt.Errorf("Wrapper.Unwrap: provided function must only have one parameter. The type of which matches that of which is being unwrapped")
+		return
+	}
+	if fun.Type().In(0).Kind() != reflect.Ptr {
+		// unsupported parameter.
+		err = fmt.Errorf("Wrapper.Unwrap: Provided function's parameter is not a pointer, which means it is being passed by value which is pointless")
+		return
+	}
+
+	if fun.Type().In(0) != value.Addr().Type() {
+		// unsupported parameter.
+		err = fmt.Errorf("Wrapper.Unwrap: provided function's parameter does not match that which is being unwrapped")
+		return
+	}
+
+	return
+}
+
+func injectFunction(value reflect.Value, f interface{}) (err error) {
+	// nil check just incase.
+	if f == nil {
+		return
+	}
+
+	reflect.ValueOf(f).Call([]reflect.Value{value.Addr()})
+
+	return
+}
